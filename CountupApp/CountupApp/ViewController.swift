27421d947647c8531f58678b88a3403d3becb195
Miyu Sato <62976492+miyu0001@users.noreply.github.com>
2021-07-08 00:59:58 +0900

    機能完成

--- CountupApp/CountupApp/ViewController.swift
+++ CountupApp/CountupApp/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count:Int = 0
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var kakeruButton: UIButton!
+    @IBOutlet weak var waruButton: UIButton!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var clearLabel: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func plus(_ sender: Any) {
+        count += 1
+        countLabel.text = String(count)
+    }
+    
+    @IBAction func minus(_ sender: Any) {
+        count -= 1
+        countLabel.text = String(count)
+    }
+    
+    @IBAction func kakeru(_ sender: Any) {
+        count = count * 2
+        countLabel.text = String(count)
+    }
+    
+    @IBAction func waru(_ sender: Any) {
+        count = count / 2
+        countLabel.text = String(count)
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+        count = 0
+        countLabel.text = String(count)
+        
+    }
 }
 
